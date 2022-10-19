e91d6101daca6b8d9aa2904f17fb4fb0683fa33a
michaeltannusa <michael.tannusa0312@gmail.com>
2022-10-19 13:49:40 +0700

    Add Ingredients Script

---
+++ Assets/Scripts/Item/Ingredients.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredients : Item
+{
+    public override void MainAction()
+    {
+        Debug.Log("Ingredientss");
+    }
+}
