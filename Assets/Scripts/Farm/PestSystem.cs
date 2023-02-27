5d9241f5e5a15656854bab84101cf85f6cbc9bf4
michaeltannusa <michael.tannusa0312@gmail.com>
2023-02-27 15:23:37 +0700

    Add Playerpref to IsAlreadyRandom,Day1,Day2

--- Assets/Scripts/Farm/PestSystem.cs
+++ Assets/Scripts/Farm/PestSystem.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System;
 
 public class PestSystem : MonoBehaviour, ITimeTracker
 {
@@ -16,6 +17,11 @@ public class PestSystem : MonoBehaviour, ITimeTracker
         m_timeManager = TimeManager.Instance;
         m_timeManager.RegisterListener(this);
 
+        // Load Boolean
+        IsAlreadyRandom = Convert.ToBoolean(PlayerPrefs.GetString("IsAlreadyRandom", "false"));
+        IsAlreadyTriggerDay1 = Convert.ToBoolean(PlayerPrefs.GetString("IsAlreadyTriggerDay1", "false"));
+        IsAlreadyTriggerDay2 = Convert.ToBoolean(PlayerPrefs.GetString("IsAlreadyTriggerDay2", "false"));
+
         Debug.Log("Pest Day : " + PlayerPrefs.GetInt("day1") + " , " + PlayerPrefs.GetInt("day2"));
     }
 
@@ -26,28 +32,20 @@ public class PestSystem : MonoBehaviour, ITimeTracker
         // every new month generate 2 days to be pest day
         if (timeStamp.day == 1 && !IsAlreadyRandom)
         {
-            PlayerPrefs.SetInt("day1", Random.Range(2, 15));
-            PlayerPrefs.SetInt("day2", Random.Range(16, 30));
+            PlayerPrefs.SetInt("day1", UnityEngine.Random.Range(2, 15));
+            PlayerPrefs.SetInt("day2", UnityEngine.Random.Range(16, 30));
 
             Debug.Log("Pest Day1 : " + PlayerPrefs.GetInt("day1") + " , Pest Day 2 : " + PlayerPrefs.GetInt("day2"));
             IsAlreadyRandom = true;
             IsAlreadyTriggerDay1 = false;
             IsAlreadyTriggerDay2 = false;
+            // Save boolean
+            PlayerPrefs.SetString("IsAlreadyRandom", IsAlreadyRandom.ToString());
+            PlayerPrefs.SetString("IsAlreadyTriggerDay1", IsAlreadyTriggerDay1.ToString());
+            PlayerPrefs.SetString("IsAlreadyTriggerDay2", IsAlreadyTriggerDay2.ToString());
         }
         else if (timeStamp.day == PlayerPrefs.GetInt("day1") && IsAlreadyTriggerDay1 == false) PestDay(true);
         else if (timeStamp.day == PlayerPrefs.GetInt("day2") && IsAlreadyTriggerDay2 == false) PestDay(false);
-
-        /*  else if( (timeStamp.day == PlayerPrefs.GetInt("day1") || timeStamp.day == PlayerPrefs.GetInt("day2")) && IsAlreadyTrigger == false )
-          {
-              // Pest Day
-              foreach(Transform childTf in tileParent)
-              {
-                  childTf.GetComponent<PlantTile>().TimeManager.UnRegisterListener(childTf.GetComponent<ITimeTracker>());
-                  Destroy(childTf.gameObject);
-              }
-              IsAlreadyRandom = false;
-              StartCoroutine("SetPestDayUI");
-          }*/
     }
 
     private void PestDay(bool IsDay1)
@@ -64,17 +62,35 @@ public class PestSystem : MonoBehaviour, ITimeTracker
 
             StartCoroutine(SetPestDayUI());
 
-            if (IsDay1) IsAlreadyTriggerDay1 = true;
-            else IsAlreadyTriggerDay2 = true;
-
+            if (IsDay1)
+            {
+                IsAlreadyTriggerDay1 = true;
+                PlayerPrefs.SetString("IsAlreadyTriggerDay1", IsAlreadyTriggerDay1.ToString());
+            }
+            else
+            {
+                IsAlreadyTriggerDay2 = true;
+                PlayerPrefs.SetString("IsAlreadyTriggerDay2", IsAlreadyTriggerDay2.ToString());
+            }
             IsAlreadyRandom = false;
+            PlayerPrefs.SetString("IsAlreadyRandom", IsAlreadyRandom.ToString());
         }
         else
         {
-            if (IsDay1) IsAlreadyTriggerDay1 = true;
-            else IsAlreadyTriggerDay2 = true;
+            if (IsDay1)
+            {
+                IsAlreadyTriggerDay1 = true;
+                PlayerPrefs.SetString("IsAlreadyTriggerDay1", IsAlreadyTriggerDay1.ToString());
+            }
+            else
+            {
+                IsAlreadyTriggerDay2 = true;
+                PlayerPrefs.SetString("IsAlreadyTriggerDay2", IsAlreadyTriggerDay2.ToString());
+            }
 
             Debug.Log("Survive from PestDay :)");
+            IsAlreadyRandom = false;
+            PlayerPrefs.SetString("IsAlreadyRandom", IsAlreadyRandom.ToString());
         }
     }
 
